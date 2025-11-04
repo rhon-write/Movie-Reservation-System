13a4d6b8aa56e5052af18a3e5824946c0032d6ca
PR1VATEDOSS <carlojaya.boni16@gmail.com>
2025-11-04 13:25:04 +0800

    not done yet

--- Movies/routes/web.php
+++ Movies/routes/web.php
@@ -37,11 +37,6 @@ Route::middleware(['customer'])->prefix('booking')->group(function () {
     Route::get('/confirmation/{bookingId}', [BookingController::class, 'confirmation'])->name('booking.confirmation');
 });
 
-/*
-|--------------------------------------------------------------------------
-| Admin Routes
-|--------------------------------------------------------------------------
-*/
 
 Route::prefix('admin')->group(function () {
     Route::get('/login', [AdminAuthController::class, 'showLogin'])->name('admin.login');
