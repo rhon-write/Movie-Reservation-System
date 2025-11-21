104e63ac08e6786c66b31bb46ee948b9c80aea8d
christopher-resgonio <resgoniochristopher80@gmail.com>
2025-11-21 17:29:46 +0800

    done

---
+++ app/Mail/BookingApproved.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace App\Mail;
+
+use App\Models\Booking;
+use Illuminate\Bus\Queueable;
+use Illuminate\Mail\Mailable;
+use Illuminate\Mail\Mailables\Content;
+use Illuminate\Mail\Mailables\Envelope;
+use Illuminate\Queue\SerializesModels;
+
+class BookingApproved extends Mailable
+{
+    use Queueable, SerializesModels;
+
+    public $booking;
+
+    public function __construct(Booking $booking)
+    {
+        $this->booking = $booking;
+    }
+
+    public function envelope(): Envelope
+    {
+        return new Envelope(
+            subject: 'Booking Approved - MovieServe',
+        );
+    }
+
+    public function content(): Content
+    {
+        return new Content(
+            view: 'emails.booking-approved',
+        );
+    }
+
+    public function attachments(): array
+    {
+        return [];
+    }
+}
